dd42f54fab580089162d7989444aee1015a76ffd
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-24 16:28:31 +0100

    dot navigation

---
+++ lib/auth/views/onboarding/widgets/dot_navigation.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:jetour_client/auth/controllers/onboarding_controller.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class DotNavigation extends StatelessWidget {
+  const DotNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = OnboardingController.instance;
+
+    return SmoothPageIndicator(
+      controller: controller.pageController,
+      onDotClicked: controller.dotNavigationClick,
+      count: 3,
+      effect: const ExpandingDotsEffect(
+        activeDotColor: Colors.blue,
+        dotHeight: 8,
+        dotWidth: 10,
+        spacing: 4,
+      ),
+    );
+  }
+}
