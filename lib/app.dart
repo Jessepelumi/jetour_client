8a6dfc3eedd811917e64f47871dff54d5c57b391
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-24 16:30:47 +0100

    main app

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:jetour_client/auth/views/onboarding/onboarding.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Jetour Payment',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const Scaffold(),
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Onboarding(),
     );
   }
 }
