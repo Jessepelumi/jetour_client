5fac15ea6898c2868ffc9b9297bfe3971fce821e
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-24 20:58:39 +0100

    modified color utilities

--- lib/core/util/colors.dart
+++ lib/core/util/colors.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-// neutral colors
+// neutral
 const white = Color(0xFFFFFFFF);
-const black = Color(0xFF171817);
+const black = Color(0xFF001029);
 
 // grey
 const darkGrey = Color(0xFF45474B);
-const lightGrey = Color(0xFFF5F5F5);
+const lightGrey = Color(0xFF999999);
 const softGrey = Color(0xFFB4B4B8);
 
-// favorite
-const gold = Color(0xFFFFC100);
+// app
+const primary = Color(0xFF0062F6);
+const activeButton = Color(0xFF0057DB);
