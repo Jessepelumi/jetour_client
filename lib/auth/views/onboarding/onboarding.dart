46c535d04244712e14b1d601802fda715e59029a
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-25 11:37:11 +0100

    implemented dot navigation and greeting in onboarding

--- lib/auth/views/onboarding/onboarding.dart
+++ lib/auth/views/onboarding/onboarding.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/instance_manager.dart';
 import 'package:jetour_client/auth/controllers/onboarding_controller.dart';
 import 'package:jetour_client/auth/views/onboarding/onboarding_view.dart';
+import 'package:jetour_client/auth/views/onboarding/widgets/dot_navigation.dart';
+import 'package:jetour_client/auth/views/onboarding/widgets/onboarding_greeting.dart';
+import 'package:jetour_client/core/util/colors.dart';
 
 class Onboarding extends StatelessWidget {
   const Onboarding({super.key});
@@ -12,10 +15,24 @@ class Onboarding extends StatelessWidget {
 
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(18),
+        padding: const EdgeInsets.all(18),
         child: SafeArea(
           child: Stack(
             children: [
+              Row(
+                children: [
+                  Image.asset("assets/image/app-icon.png"),
+                  const SizedBox(width: 4),
+                  Text(
+                    "JETOUR",
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: primary,
+                        ),
+                  ),
+                ],
+              ),
+
               // horizontal scrollable pages
               PageView(
                 controller: controller.pageController,
@@ -23,24 +40,33 @@ class Onboarding extends StatelessWidget {
                 children: const [
                   OnboardingView(
                     image: "assets/image/contactless.png",
-                    title: "Make payments anytime",
+                    title: "Make Payments Anytime, Anywhere",
                     subtitle:
                         "Send and receive money even without internet with our offline NFC payment solution, keeping you connected to your finances, no matter where you are.",
+                    buttonText: "Continue",
                   ),
                   OnboardingView(
                     image: "assets/image/shield-locked.png",
                     title: "Your Payments, Fully Protected",
                     subtitle:
                         "Every transaction is secured with encryption and tokenization, ensuring your data stays safe. Biometric and PIN verification keeps your account secure!",
+                    buttonText: "Continue",
                   ),
                   OnboardingView(
                     image: "assets/image/sync-lock.png",
                     title: "Seamless Syncing, Hassle-Free",
                     subtitle:
                         "Offline payments are automatically syncned when you're back online, so you never miss a beat. Get real-time notifications once transactions get synced.",
+                    buttonText: "Get Started",
                   ),
                 ],
               ),
+
+              // dot
+              const DotNavigation(),
+
+              // hello
+              const OnboardingGreeting(),
             ],
           ),
         ),
