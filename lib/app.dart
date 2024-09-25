22e34d8005dc9354350b89c93a8694fc60b1e2d9
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-25 11:37:40 +0100

    added elevation button theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jetour_client/auth/views/onboarding/onboarding.dart';
+import 'package:jetour_client/core/theme/elevated_button.dart';
+import 'package:jetour_client/core/util/colors.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Onboarding(),
+      theme: ThemeData(
+        primaryColor: primary,
+        elevatedButtonTheme: AppElevatedButtonTheme.appElevatedButtonTheme,
+      ),
+      home: const Onboarding(),
     );
   }
 }
