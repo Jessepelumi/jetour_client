25eea392284db2fd911e066cd6d2c0cc83a8a4ea
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-25 11:35:16 +0100

    changed color and position height

--- lib/auth/views/onboarding/widgets/dot_navigation.dart
+++ lib/auth/views/onboarding/widgets/dot_navigation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jetour_client/auth/controllers/onboarding_controller.dart';
+import 'package:jetour_client/core/util/colors.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class DotNavigation extends StatelessWidget {
@@ -10,13 +11,13 @@ class DotNavigation extends StatelessWidget {
     final controller = OnboardingController.instance;
 
     return Positioned(
-      top: MediaQuery.of(context).size.height * 0.5,
+      top: MediaQuery.of(context).size.height * 0.48,
       child: SmoothPageIndicator(
         controller: controller.pageController,
         onDotClicked: controller.dotNavigationClick,
         count: 3,
         effect: const ExpandingDotsEffect(
-          activeDotColor: Colors.blue,
+          activeDotColor: primary,
           dotHeight: 8,
           dotWidth: 10,
           spacing: 4,
