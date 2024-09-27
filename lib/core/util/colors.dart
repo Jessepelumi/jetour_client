1e509d2b822a0538dde8acd9e7afecf0aeeeb9c2
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-27 14:04:38 +0100

    extracted pin code input with label

--- lib/core/util/colors.dart
+++ lib/core/util/colors.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 // neutral
 const white = Color(0xFFFFFFFF);
 const black = Color(0xFF001029);
+const black2 = Color(0xFF111111);
 
 // grey
 const darkGrey = Color(0xFF45474B);
 const lightGrey = Color(0xFF999999);
-const softGrey = Color(0xFFB4B4B8);
 
 // app
 const primary = Color(0xFF0062F6);
---
+++ lib/shared/pincode_input.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:jetour_client/core/util/colors.dart';
+import 'package:jetour_client/core/util/sizes.dart';
+import 'package:jetour_client/shared/pin_field.dart';
+
+class PinCodeInput extends StatelessWidget {
+  const PinCodeInput({
+    super.key,
+    required this.label,
+    this.isRequired = true,
+    required this.pinInputLength,
+    required this.action,
+  });
+
+  final String label;
+  final bool? isRequired;
+  final int pinInputLength;
+  final VoidCallback action;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Text(label),
+            if (isRequired == true)
+              Text(
+                " *",
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge
+                    ?.copyWith(color: red, fontSize: smallMedium),
+              ),
+          ],
+        ),
+        const SizedBox(height: extraSmall),
+        PinInputScreen(
+          lenght: pinInputLength,
+          obscure: false,
+          onComplete: action,
+        ),
+      ],
+    );
+  }
+}
