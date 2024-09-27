ea543b303c2f08da2c4cea376adb4faa3b54683a
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-27 14:08:26 +0100

    extracted information container

---
+++ lib/shared/information_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:jetour_client/core/util/sizes.dart';
+
+class InformationContainer extends StatelessWidget {
+  const InformationContainer({
+    super.key, required this.label, required this.color,
+  });
+
+  final String label;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: small,
+        vertical: extraSmall,
+      ),
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.3),
+      ),
+      child: Text(
+        label,
+        style: Theme.of(context).textTheme.labelMedium?.copyWith(color: color),
+      ),
+    );
+  }
+}
