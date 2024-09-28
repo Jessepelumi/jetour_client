8d0584fbe2f59817f4213719ac9e5b1276463a23
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-28 05:35:01 +0100

    extracted home header widget

---
+++ lib/views/home/widgets/home_header.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:jetour_client/core/util/colors.dart';
+import 'package:jetour_client/core/util/sizes.dart';
+import 'package:jetour_client/shared/information_container.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: primary,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Center(
+                child: Text(
+                  "JA",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(color: white, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            const SizedBox(width: small),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Good morning,"),
+                Text(
+                  "Jesse",
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelLarge
+                      ?.copyWith(fontWeight: FontWeight.bold, fontSize: medium),
+                ),
+              ],
+            )
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              "Internet status:",
+              style: Theme.of(context).textTheme.labelSmall,
+            ),
+            const InformationContainer(label: "Online", color: green),
+          ],
+        ),
+      ],
+    );
+  }
+}
