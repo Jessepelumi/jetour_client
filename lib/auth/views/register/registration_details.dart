6cf87f4e2a077ae92106d7abf18566391a6deaba
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-09 22:04:25 +0100

    modified the structure of the registration details page

--- lib/auth/views/register/registration_details.dart
+++ lib/auth/views/register/registration_details.dart
@@ -1,125 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:jetour_client/auth/views/register/widgets/registration_textfield.dart';
 import 'package:jetour_client/auth/views/register/widgets/terms_privacy.dart';
 import 'package:jetour_client/auth/views/shared/login_link.dart';
 import 'package:jetour_client/core/util/colors.dart';
 import 'package:jetour_client/core/util/sizes.dart';
+import 'package:jetour_client/router/navigation.dart';
+import 'package:jetour_client/shared/custom_appbar.dart';
+import 'package:jetour_client/shared/information_container.dart';
 import 'package:jetour_client/shared/jetour_elevated_button.dart';
 
-class RegistrationDetails extends StatefulWidget {
+class RegistrationDetails extends StatelessWidget {
   const RegistrationDetails({super.key});
-
-  @override
-  RegistrationDetailsState createState() => RegistrationDetailsState();
-}
-
-class RegistrationDetailsState extends State<RegistrationDetails> {
-  bool _isLoading = false;
-
-  void _onContinue() async {
-    setState(() {
-      _isLoading = true; // Start loading
-    });
-
-    // Simulate a network request with a delay
-    await Future.delayed(const Duration(seconds: 3));
-
-    // Navigate to the next screen after the delay
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (context) =>
-              const NextScreen()), // Replace with your next screen
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        title: const Text("Create an account"),
-        centerTitle: false,
-      ),
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: medium),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Sign up for an account and get access to Jetour's features",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyMedium
-                        ?.copyWith(color: darkGrey),
-                  ),
-                  const SizedBox(height: small),
-
-                  // Full Name
-                  const RegistrationTextField(
-                    label: "Full Name",
-                    placeholder: "Enter full name here",
-                  ),
-                  const SizedBox(height: small),
-                  const RegistrationTextField(
-                    label: "Bank Verification Number (BVN)",
-                    placeholder: "Enter 10 digits BVN",
-                  ),
-                  const SizedBox(height: small),
-                  const RegistrationTextField(
-                    label: "National Identity Number",
-                    placeholder: "Enter NIN Number",
-                    isRequired: false,
-                  ),
-                  const SizedBox(height: small),
-
-                  // login link
-                  const LoginLink(),
-                ],
-              ),
-            ),
-          ),
-
-          // Loading overlay
-          if (_isLoading)
-            Container(
-              color: Colors.black54, // Semi-transparent background
-              width: double.infinity, // Fill the width
-              height: double.infinity, // Fill the height
-              child: const Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircularProgressIndicator(),
-                    Text('Verifying Your Details'),
-                  ],
-                ), // Loading indicator
-              ),
-            ),
-        ],
-      ),
-
-      // bottom
-      bottomNavigationBar: SafeArea(
+      appBar: const CustomAppBar(title: "Create an account"),
+      body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: medium,
-            vertical: smallMedium,
-          ),
+          padding: const EdgeInsets.symmetric(horizontal: medium),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
             children: [
-              const TermsAndPrivacy(),
-              const SizedBox(height: smallMedium),
-              JetourElevatedButton(
-                buttonAction: _isLoading
-                    ? () {}
-                    : _onContinue, // Use no-op function when loading
-                buttonText: _isLoading ? "Loading..." : "Continue",
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Welcome! Just enter a few personal details to complete your account setup.",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(color: darkGrey),
+                      ),
+                      const SizedBox(height: small),
+
+                      // email address
+                      const RegistrationTextField(
+                        label: "Full Name",
+                        placeholder: "Enter full name here",
+                      ),
+                      const SizedBox(height: small),
+                      const RegistrationTextField(
+                        label: "Bank Verification Number (BVN)",
+                        placeholder: "Enter 10 digits BVN",
+                      ),
+                      const SizedBox(height: small),
+                      const InformationContainer(
+                        label: "JESSE ADESINA",
+                        color: green,
+                      ),
+                      const SizedBox(height: small),
+                      const RegistrationTextField(
+                        label: "National Identity Number (NIN)",
+                        placeholder: "Enter NIN Number",
+                        isRequired: false,
+                      ),
+                    ],
+                  ),
+                ),
               ),
+              SizedBox(
+                width: double.infinity,
+                child: JetourElevatedButton(
+                  buttonText: "Continue",
+                  buttonAction: () => Get.to(() => const NextScreen()),
+                ),
+              ),
+              const SizedBox(height: small),
             ],
           ),
         ),
@@ -136,8 +85,9 @@ class NextScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Create an account",
-        style: TextStyle(fontWeight: FontWeight.bold),
+        title: const Text(
+          "Create an account",
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
       body: Center(
@@ -187,7 +137,9 @@ class NextScreen extends StatelessWidget {
             children: [
               const SizedBox(height: smallMedium),
               JetourElevatedButton(
-                buttonAction: () {}, // Use no-op function when loading
+                buttonAction: () => Get.to(
+                  () => const Navigation(),
+                ), // Use no-op function when loading
                 buttonText: "Continue",
               ),
             ],
